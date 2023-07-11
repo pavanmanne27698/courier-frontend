73802cc75d7e2c92ba156261fb766a4659628063
Reddynaveena <132968556+Reddynaveena@users.noreply.github.com>
2023-07-11 23:47:53 +0530

    Add vue page showing the list of orders

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,19 +1,207 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, watch } from "vue";
 import { useRouter } from "vue-router";
+import OrderServices from "../services/OrderServices.js";
+import { ref } from "vue";
+import Loading from "../components/Loading.vue";
 import LeftNavbar from "../components/LeftNavbar.vue";
+import { getUrl } from "../common"
 
+const orders = ref([]);
+const isLoading = ref(true);
+const user = ref(null);
 const router = useRouter();
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+const actualResponse = ref([])
+const sort = ref(0)
+const search = ref("")
+const options = [
+  { value: 0, label: 'All' },
+  { value: 1, label: 'Pending' },
+  { value: 2, label: 'Delivered' },
+  { value: 3, label: 'Progress' },
+  { value: 4, label: 'Not Assigned' },
+];
 onMounted(async () => {
-  if (localStorage.getItem("user") == null) {
+   user.value = JSON.parse(localStorage.getItem("user"));
+  if(!user.value) {
     router.push({ name: "login" });
   }
+  await getOrders();
+  isLoading.value = false;
 });
+
+const setSnackbar = (text,color="error") => {
+    snackbar.value.value = true;
+    snackbar.value.color = color;
+    snackbar.value.text = text;
+}
+
+async function getOrders() {
+  await OrderServices.getOrders()
+    .then((res) => {
+      orders.value = res.data;
+      actualResponse.value = res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+const deleteOrder = async(id,index) => {
+
+    await OrderServices.deleteOrder(id)
+    .then((res) => {
+      orders.value.splice(index, 1);
+      setSnackbar("Customer is deleted successfully!","green")
+    })
+    .catch((error) => {
+      console.log(error);
+      setSnackbar(error.response.data.message)
+    });
+}
+function closeSnackBar() {
+  snackbar.value.value = false;
+}
+function goToUpdatePage(id) {
+  router.push({ name: "updateOrder",params: { id } })
+}
+ watch(
+      sort,
+      (newValue, _) => {
+        var sortedOrders = actualResponse.value
+        if (newValue != 0) {
+          if(newValue ==1) {
+            sortedOrders = actualResponse.value.filter(order => order.status.toLowerCase() == "pending");
+          } else if(newValue == 2) {
+            sortedOrders = actualResponse.value.filter(order => order.status.toLowerCase() == "progress");
+          } else if(newValue == 3) {
+            sortedOrders = actualResponse.value.filter(order => order.status.toLowerCase() == "delivered");
+          } else if(newValue == 4) {
+            sortedOrders = actualResponse.value.filter(order => !order.deliveryBoyUserId);
+          }
+        } 
+        orders.value = sortedOrders
+      }
+    );
+
+ watch(
+      search,
+      (newValue, oldValue) => {
+        if(!newValue) {
+          orders.value = actualResponse.value
+          return;
+        }
+        const filteredOrders = actualResponse.value.filter(order => 
+        order.id == newValue
+        );
+        orders.value = filteredOrders;
+      }
+    );
+  
 </script>
 
 <template>
-    <div class="flex flex-1">
-        <LeftNavbar/>
-        <p>Order</p>
-    </div>
+<v-container>
+   <div class="flex flex-1">
+      <LeftNavbar/>
+    <div class="container" style="margin-top: 20px">
+      <div style="display: flex; justify-content: center;">
+        <h3>Orders</h3>
+      <a class="btn btn-danger create" :href="[getUrl() + '/add-order']" style="margin-left:10px;" >Add Order</a>
+      </div>
+      <br/>
+      <div style="display:flex;margin-top:5px;margin-bottom:20px;">
+        <div>
+          <select class="form-control" id="dropdown" style="margin-left:20px;" v-model="sort">
+            <option v-for="(option, index) in options" :key="index" :value="option.value">
+              {{ option.label }}
+            </option>
+          </select>
+        </div>
+        <input class="form-control searchbar" type="search" placeholder="Search by Order id" aria-label="Search" v-model="search"/><br/>
+      </div>
+      <Loading v-if="isLoading" />
+      <div class="col-md-12 container elevation-4 orders" v-else-if="orders.length != 0">
+              <table class="table" style="background-color: white;">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Pickup Customer</th>
+                    <th scope="col">Delivery Customer</th>
+                    <th v-if="user.role_id != 3"> Delivery Boy </th>
+                    <th scope="col">Pickup Time</th>
+                    <th scope="col">Price of Order</th>
+                    <th scope="col">Distance</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" v-if="user.role_id != 3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(order,index) in orders" :key="index" >
+                    <th scope="row">{{ order.id }}</th>
+                    <td>{{ order.pickupCustomer.firstName }} {{ order.pickupCustomer.lastName }}</td>
+                    <td>{{ order.deliveryCustomer.firstName }} {{ order.deliveryCustomer.lastName }}</td>
+                    <td v-if="order.deliveryBoyUserId">{{ order.deliveryBoyUser.firstName }} {{ order.deliveryBoyUser.laststName }}</td>
+                    <td v-else-if="user.role_id != 3"> Not Assigned</td>
+                    <td>{{ order.pickupTime }}</td>
+                    <td>${{ order.cost }}</td>
+                    <td >{{ order.distance }} Miles</td>
+                    <td> {{ order.status.toLowerCase() }} </td>
+                    <td v-if="user.role != 3">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                              <img class="button-image" :src="[getUrl()+'/edit.png']" width="20" height="20" @click="goToUpdatePage(id)" />
+                              <img class="button-image" src="/delete.png" width="20" height="20" @click="deleteOrder(order.id,index)" />
+                          </div>      
+                    </td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        <div class="text-center" v-else>
+        <h4 class="text-muted">No orders Found!</h4>
+      </div>
+      </div>
+   </div>
+     <v-snackbar v-model="snackbar.value" rounded="pill">
+          {{ snackbar.text }}
+
+          <template v-slot:actions>
+            <v-btn
+              :color="snackbar.color"
+              variant="text"
+              @click="closeSnackBar()"
+            >
+              Close
+            </v-btn>
+          </template>
+        </v-snackbar>
+  <br/>
+</v-container>
+
 </template>
+
+<style scoped>
+
+.orders {
+  padding: 30px;
+  background-color: white;
+}
+.create {
+    background-color: #80162B ;
+    margin-left: 20px;
+    padding-top: -10px;
+    color: white;
+}
+.searchbar {
+  margin-left: 3%;
+  width: 70%;
+}
+.button-image {
+  cursor: pointer;
+}
+</style>
